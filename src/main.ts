c0cbda645263b82be1b16892984e59826777defb
devkoalaa <herberticleber@gmail.com>
2025-09-06 15:18:07 -0300

    deploy config

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express'; // NOVO: Importar o adapter
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express'; // NOVO: Importar o express
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common';
+import express = require('express');
 
-// NOVO: Criar e exportar a instância do Express que a Vercel usará
 export const app = express();
 
 async function bootstrap() {
-  // MUDOU: O NestJS agora usa a instância do Express que criamos
   const server = await NestFactory.create(
     AppModule,
     new ExpressAdapter(app),
   );
 
-  // A partir daqui, usamos a variável 'server' em vez de 'app'
   server.enableCors({
     origin: ['http://localhost:5757', 'http://localhost:3000', 'https://cs2smokeshub.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -61,18 +58,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(server, config);
-  SwaggerModule.setup('api-docs', server, document, { // Mudei a rota para 'api-docs' para evitar conflitos
+  SwaggerModule.setup('api-docs', server, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
   });
   // }
 
-  // MUDOU: Usamos init() para inicializar o app sem escutar uma porta
   await server.init();
-
-  // REMOVIDO: A seção app.listen(port) e os console.log foram removidos
 }
 
-// A função é chamada para garantir que a configuração seja executada
 bootstrap();
